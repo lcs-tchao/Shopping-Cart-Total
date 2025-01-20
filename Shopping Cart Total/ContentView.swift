067258bbba16d26b38c46e3d489c349a3a834892
Thomas Chao <Tchao@lcs.on.ca>
2025-01-20 09:01:53 -0500

    Initial Commit

---
+++ Shopping Cart Total/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shopping Cart Total
+//
+//  Created by Yuk Yeung Chao on 2025-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shopping Cart Total/Shopping_Cart_TotalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_Cart_TotalApp.swift
+//  Shopping Cart Total
+//
+//  Created by Yuk Yeung Chao on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_Cart_TotalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
